ffeb9ade99f98d9e65a1ee9d412862e27ea5039f
Widia Sabrina <widiasabrina875@yahoo.co.id>
2019-01-03 11:39:06 +0700

    Biodata full, clazz full, batch almost full

---
+++ XBC/XBC.MVC/Controllers/BatchController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XBC.Repo;
+using XBC.ViewModel;
+
+namespace XBC.MVC.Controllers
+{
+    public class BatchController : Controller
+    {
+        // GET: Batch
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            //ViewBag.BatchTestId = BTID;
+            return PartialView("_List", BatchRepo.All());
+        }
+
+        public ActionResult Search(string searching)
+        {
+            return PartialView("_List", BatchRepo.search(searching));
+        }
+
+        //get BTID on _Test View
+        //public ActionResult GetBT(long BID,long BTID)
+        //{
+        //    ViewBag.BatchTestId = BTID;
+        //    return PartialView("_List", BatchRepo.ByBT(BID,BTID));
+        //}
+
+        public ActionResult Create()
+        {
+            ViewBag.TechnologyList = new SelectList(TechnologyRepo.ByTechnology(), "id", "name");
+            ViewBag.TrainerList = new SelectList(TrainerRepo.ByTrainer(), "id", "name");
+            ViewBag.RoomList = new SelectList(RoomRepo.All(),"id","name");
+            ViewBag.BTList = new SelectList(BTRepo.GetAll(),"id","name");
+            return PartialView("_Create", new BatchViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(BatchViewModel model)
+        {
+            ResponseResult result = BatchRepo.CreateEdit(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BatchViewModel model = BatchRepo.ById(id);
+            ViewBag.TechnologyList = new SelectList(TechnologyRepo.ByTechnology(), "id", "name");
+            ViewBag.TrainerList = new SelectList(TrainerRepo.ByTrainer(), "id", "name");
+            ViewBag.RoomList = new SelectList(RoomRepo.All(), "id", "name");
+            ViewBag.BTList = new SelectList(BTRepo.GetAll(), "id", "name");
+            return PartialView("_Edit", model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BatchViewModel model)
+        {
+            ResponseResult result = BatchRepo.CreateEdit(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        //Setup Test (Tabel Batch)
+        public ActionResult Test(long? id, long? bt)
+        {
+            //id => Batch Test Id
+            //ViewBag.BatchTestId = bt;
+            //id => Batch Id
+            ViewBag.BatchId = id;
+            ViewBag.BatchTestId = new SelectList(BatchTestRepo.ByBatchTest(bt));
+            return PartialView("_Test", TestRepo.GetBySearch(id/*, bt*/));
+        }
+
+        public ActionResult CreateBT(int testId, int batchId)
+        {
+            //id => Batch Test Id
+            //ViewBag.BatchTestId = id;
+            return PartialView("_Test",new BatchTestViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult CreateBT(BatchTestViewModel model)
+        {
+            ResponseResult result = BatchRepo.SaveBT(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteBT(int id)
+        {
+            return PartialView("_Test", BatchTestRepo.ById(id));
+        }
+
+        [HttpPost]
+        public ActionResult DeleteBT(BatchTestViewModel model)
+        {
+            ResponseResult result = BatchTestRepo.Delete(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        //public ActionResult Test(string search = " ")
+        //{
+        //    return PartialView("_Test", TestRepo.GetBySearch(search));
+        //}
+
+        //public ActionResult CreateParticipant()
+        //{
+        //    ViewBag.BiodataList = new SelectList(BiodataRepo.All(), "id", "name");
+        //    return PartialView("_AddParticipant", new BatchViewModel());
+        //}
+
+        //[HttpPost]
+        //public ActionResult CreateParticipant(ClassViewModel model)
+        //{
+        //    ResponseResult result = ClassRepo.CreateParticipant(model);
+        //    return Json(new
+        //    {
+        //        success = result.Success,
+        //        message = result.Message,
+        //        entity = result.Entity
+        //    }, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
---
+++ XBC/XBC.MVC/Controllers/BiodataController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XBC.DataModel;
+using XBC.Repo;
+using XBC.ViewModel;
+
+namespace XBC.MVC.Controllers
+{
+    public class BiodataController : Controller
+    {
+        //XBCContext db = new XBCContext();
+        // GET: Biodata
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return PartialView("_List", BiodataRepo.All());
+        }
+
+        //public ActionResult Search(string s)
+        //{
+        //    List<BiodataViewModel> model = BiodataRepo.search(s);
+        //    return View("Search", model);
+        //}
+
+        public ActionResult Search(string searching)
+        {
+            //var bio = from b in db.t_biodata
+            //          select b;
+
+            //if (!string.IsNullOrEmpty(searching))
+            //{
+            //    bio = bio.Where(b => b.name.Contains(searching) || searching == null);
+            //}
+
+            //return View("Search",bio.ToList());
+
+            //return View(db.t_biodata.Where(b => b.name.StartsWith(searching) || searching == null).ToList());
+
+            return PartialView("_List",BiodataRepo.search(searching));
+            
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView("_Create",new BiodataViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(BiodataViewModel model)
+        {
+            ResponseResult result = BiodataRepo.CreateEdit(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BiodataViewModel model = BiodataRepo.ById(id);
+            ViewBag.BiodataList = new SelectList(BTTRepo.GetAll(), "id", "name");
+            return PartialView("_Edit",model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BiodataViewModel model)
+        {
+            ResponseResult result = BiodataRepo.CreateEdit(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return PartialView("_Delete", BiodataRepo.ById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(BiodataViewModel model)
+        {
+            ResponseResult result = BiodataRepo.Delete(model);
+            return Json(new
+            {
+                success = result.Success,
+                message = result.Message,
+                entity = result.Entity
+            }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}

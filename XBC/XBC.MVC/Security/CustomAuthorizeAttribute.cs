612d9c597a0710b463500ceb84208f0024b6cd78
Dikri Syarif <dikrisyarif766@gmail.com>
2019-01-03 10:55:06 +0700

    add login, crud user, crud menu, crud menu acses

---
+++ XBC/XBC.MVC/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,41 @@
+﻿using XBC.Repo;
+using XBC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using XBC.MVC.WebApp.Security;
+
+namespace XBC.MVC.WebApp.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        //public CustomAuthorizeAttribute()
+        //{
+        //    AccessRight = string.Empty;
+        //}
+
+        //public string AccessRight { get; set; }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(SessionPersister.UserName))
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Account", action = "Index" }));
+            }
+            else
+            {
+                AccountViewModel avm = new AccountViewModel();
+                avm.username = SessionPersister.UserName;
+                CustomPrincipal mp = new CustomPrincipal(avm.find(SessionPersister.UserName));
+                if (!mp.IsInRole(Roles))
+                {
+                    filterContext.Result = new RedirectToRouteResult(new
+                        RouteValueDictionary(new { controller = "AccessDenied", action = "Index" }));
+                }
+            }
+        }
+    }
+}
---
+++ XBC/XBC.MVC/Security/SessionPersister.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XBC.MVC.WebApp.Security
+{
+    public static class SessionPersister
+    {
+        static string varUserIdSession = "1";
+        static string varUserNameSession = "username";
+        static string varFullNameSession = "fullname";
+
+        public static string UserId {
+            get
+            {
+                if (HttpContext.Current == null)
+                    return string.Empty;
+
+                var sessionVar = HttpContext.Current.Session[varUserIdSession];
+                if (sessionVar != null)
+                    return sessionVar as string;
+                return null;
+            }
+
+            set
+            {
+                HttpContext.Current.Session[varUserIdSession] = value;
+            }
+        }
+
+        public static string UserName
+        {
+            get
+            {
+                if (HttpContext.Current == null)
+                    return string.Empty;
+
+                var sessionVar = HttpContext.Current.Session[varUserNameSession];
+                if (sessionVar != null)
+                    return sessionVar as string;
+                return null;
+            }
+
+            set
+            {
+                HttpContext.Current.Session[varUserNameSession] = value;
+            }
+        }
+
+        public static string FullName
+        {
+            get
+            {
+                if (HttpContext.Current == null)
+                    return string.Empty;
+
+                var sessionVar = HttpContext.Current.Session[varFullNameSession];
+                if (sessionVar != null)
+                    return sessionVar as string;
+                return null;
+            }
+
+            set
+            {
+                HttpContext.Current.Session[varFullNameSession] = value;
+            }
+        }
+    }
+}
